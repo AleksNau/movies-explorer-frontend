d9836e69c350e1ba61ac0f75b9ff3c0739bd3f79
AleksNau <workciti@yandex.ru>
2023-09-17 16:45:37 +0300

    сделать header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Route, Routes,useNavigate } from "react-router-dom";
 import "./Header.css";
+import logo from "../../images/logo.svg";
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
-        <div className="header">
+        <header className="header">
+            <img src={logo} alt="логотип" className="header__logo" />
+            <Routes>
+                <Route path="/index" element={<div className="header__movies-conteiner">
+                    <button className="header__button" onClick={() => {navigate("/movies")}}>Фильмы</button>
+                    <button className="header__button" onClick={() => {navigate("/saved-movies")}}>Сохраненые фильмы</button>
+                </div>}/>
+            </Routes>
+            <Routes>
+                <Route path="/" element={<div className="header__profile-conteiner">
+                    <button className="header__button header__button_sign-up" onClick={() => {navigate("/sign-up")}}>Регистрация</button>
+                    <button className="header__button header__button_sign-in" onClick={() => {navigate("/sign-in")}}>Войти</button>
+                </div>}/>
+                <Route path="/index" element={<div>
+                    <button className="header__button header__button_account" onClick={() => {navigate("/sign-up")}}>Аккаунт</button>
+                </div>}/>
+            </Routes>
 
-        </div>
+        </header>
     );
 };
 
