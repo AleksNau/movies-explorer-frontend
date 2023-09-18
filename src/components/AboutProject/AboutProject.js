f29bafdc0867398617c7e56d7e1106d69711a467
AleksNau <workciti@yandex.ru>
2023-09-18 21:12:29 +0300

    about-project 1280

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,15 +5,16 @@ const AboutProject = () => {
     return (
         <section className="about-project">
             <h2 className="about-project__title">О проекте</h2>
-            <div className="about-project__description">
-                <p className="about-project__subtitle">Дипломный проект включал 5 этапов</p>
-                <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+            <div className="about-project__conteiner">
+                <div className="about-project__description">
+                    <p className="about-project__subtitle">Дипломный проект включал 5 этапов</p>
+                    <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="about-project__description">
+                    <p className="about-project__subtitle">На выполнение диплома ушло 5 недель</p>
+                    <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
             </div>
-            <div className="about-project__description">
-                <p className="about-project__subtitle">На выполнение диплома ушло 5 недель</p>
-                <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-            </div>
-
             <div className="about-project__timeline">
                 <p className="about-project__weeks">1 неделя</p>
                 <p className="about-project__weeks">4 недели</p>
