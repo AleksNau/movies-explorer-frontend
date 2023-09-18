65716b25c427cf06d4e1ec245edbd7e8d1aefd70
AleksNau <workciti@yandex.ru>
2023-09-18 17:24:08 +0300

    about-project init

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,9 +3,24 @@ import "./AboutProject.css";
 
 const AboutProject = () => {
     return (
-        <div className="about-project">
+        <section className="about-project">
+            <h2 className="about-project__title">О проекте</h2>
+            <div className="about-project__description">
+                <p className="about-project__subtitle">Дипломный проект включал 5 этапов</p>
+                <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+            </div>
+            <div className="about-project__description">
+                <p className="about-project__subtitle">На выполнение диплома ушло 5 недель</p>
+                <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+            </div>
 
-        </div>
+            <div className="about-project__timeline">
+                <p className="about-project__weeks">1 неделя</p>
+                <p className="about-project__weeks">4 недели</p>
+                <p className="about-project__stack">Back-end</p>
+                <p className="about-project__stack">Front-end</p>
+            </div>
+        </section>
     );
 };
 
