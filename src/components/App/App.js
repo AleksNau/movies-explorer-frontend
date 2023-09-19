3f1f41555c17899560335073c9bdd64e0f7b916f
AleksNau <workciti@yandex.ru>
2023-09-19 17:40:31 +0300

    Portfolio init + adaptive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,17 +4,19 @@ import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Promo/>
-      <AboutProject/>
-       <Techs/>
-        <AboutMe/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Promo/>
+            <AboutProject/>
+            <Techs/>
+            <AboutMe/>
+            <Portfolio/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,9 +3,23 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
     return (
-        <div className="portfolio">
-
-        </div>
+        <section className="portfolio">
+            <h2 className="portfolio__title">Портфолио</h2>
+            <ul className="portfolio__list">
+                <li className="portfolio__item">
+                    <a href="https://github.com/" className="portfolio__link">Статичный сайт</a>
+                    <span className="portfolio__link-image">&#x2197;</span>
+                </li>
+                <li className="portfolio__item">
+                    <a href="https://github.com/" className="portfolio__link">Адаптивный сайт</a>
+                    <span className="portfolio__link-image">&#x2197;</span>
+                </li>
+                <li className="portfolio__item">
+                    <a href="https://github.com/" className="portfolio__link">Одностраничное приложение</a>
+                    <span className="portfolio__link-image">&#x2197;</span>
+                </li>
+            </ul>
+        </section>
     );
 };
 
