634feb77456ea8cf9a6278e6534ea85a970f8ac6
AleksNau <workciti@yandex.ru>
2023-09-20 11:24:16 +0300

    Footer init + adaptive + links animation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,14 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-
+            <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+            <div className="footer__conteiner">
+                <p className="footer__date">© 2020</p>
+                <ul className="footer__list">
+                    <li className="footer__item"><a href="https://github.com/" className="footer__link">Яндекс.Практикум</a></li>
+                    <li className="footer__item"><a href="https://practicum.yandex.ru/" className="footer__link">Github</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
