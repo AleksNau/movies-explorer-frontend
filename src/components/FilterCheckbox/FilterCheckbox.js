bda7f848fc3dd3ff940cf73f38e38d8b4023eeba
AleksNau <workciti@yandex.ru>
2023-09-20 17:37:56 +0300

    сheckbox init

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -4,7 +4,8 @@ import "./FilterCheckbox.css";
 const FilterCheckbox = () => {
     return (
         <div className="filter-сheckbox">
-
+            <input className="check" type="checkbox" name="check" id="newchec" value="Название"/>
+            <label htmlFor="newchec"></label>
         </div>
     );
 };
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./SearchForm.css";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 const SearchForm = () => {
-    return (
+    return (<>
             <form className="search-form">
                 <input type="search" placeholder="Фильм" className="search-form__input"></input>
                 <button className="search-form__submit"></button>
             </form>
+        <FilterCheckbox/>
+        </>
     );
 };
 
