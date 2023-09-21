a404077f6108f36ae86c68438342928f5b327b23
AleksNau <workciti@yandex.ru>
2023-09-21 12:52:58 +0300

    movies init

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "./MoviesCard.css";
 
-const MoviesCard = () => {
+const MoviesCard = ({cardData}) => {
     return (
-        <div className="movies-card">
-
-        </div>
+            <li className="movies-card">
+                <img
+                    src={cardData.link}
+                    alt={cardData.name}
+                    className="elements__image"
+                    onClick={() => {
+                        onCardClick(cardData);
+                    }}
+                />
+                <div className="elements__info">
+                    <p className="elements__text">{cardData.name}</p>
+                    <div className="elements__like-container">
+                        <button
+                            type="button"
+                            className={cardLikeButtonClassName}
+                            onClick={() => onCardLike(cardData)}
+                        />
+                        <p className="elements__like-counter">{likes.length}</p>
+                    </div>
+                    {isOwn && (
+                        <button
+                            type="button"
+                            className="elements__delete"
+                            onClick={() => {
+                                setActive(!isOpen);
+                                setCardToDelete(cardData);
+                            }}
+                        />
+                    )}
+                </div>
+            </li>
     );
 };
 
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./MoviesCardList.css";
+import MoviesCard from "../MoviesCard/MoviesCard";
 
-const MoviesCardList = () => {
+const MoviesCardList = ({cards}) => {
     return (
-        <div className="movies-card-list">
-
-        </div>
+            <ul className="movies-card-list">
+                {cards.map((card) => {
+                    return (
+                        <MoviesCard
+                            key={card._id}
+                            cardData={card}
+                            isOpen={isPopupSubmit}
+                            setActive={onSubmitDelete}
+                            onCardClick={onCardClick}
+                            onCardDelete={onCardDelete}
+                            setCardToDelete={setCardToDelete}
+                        />
+                    );
+                })}
+            </ul>
     );
 };
 
